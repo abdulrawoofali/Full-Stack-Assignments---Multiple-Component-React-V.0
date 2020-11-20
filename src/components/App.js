eae79c84e3c0c2af63a677c40d28f026efdb0b75
abdulrawoofali <43907143+abdulrawoofali@users.noreply.github.com>
2020-11-20 12:53:51 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
-}
-
+const App = () => {
+	return (
+		<>
+			<ul data-ns-test="project-name">
+				<h3>Book Bees</h3>
+				<li data-ns-test="project-description">
+					Book Bees is a Place Where you can find Free Book to Read. And comment
+					on books ,share reviews and write your review,share your books with
+					fellow Readers.
+				</li>
+			</ul>
+		</>
+	);
+};
 
 export default App;
